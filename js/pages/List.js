4c4b4212972ce39c48cd93c3939130c7366f9d78
Agnoxy <patrykzajac123fb@wp.pl>
2024-08-15 23:47:36 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,7 +100,7 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Achieved the record by playing the game
                     </p>
                     <p>
                         Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
@@ -109,19 +109,19 @@ export default {
                         Have either source audio or clicks/taps in the video. Edited audio only does not count
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        The recording can't contain epon06
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        The recording must also show the player hit the endwall, or the completion will be invalidated
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        Just beat the level
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        Do not hack or I will karate your ass
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Zoinks scoobs, looks like this guy is reading the submission requirements
                     </p>
                 </div>
             </div>
