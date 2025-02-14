f569ab301f8f3857d065a211d46a685289cb5b8c
Agnoxy <patrykzajac123fb@wp.pl>
2025-02-14 09:44:06 +0100

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -26,7 +26,7 @@ export default {
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
-                <div class="type-title-sm">verifier</div>
+                <div class="type-title-sm">Verifier</div>
                 <p class="type-body">
                     <span>{{ verifier }}</span>
                 </p>
