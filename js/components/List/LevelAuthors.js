494c314174c9d498a340908eb9748ed1b2ffbf5d
Agnoxy <patrykzajac123fb@wp.pl>
2025-02-14 09:47:06 +0100

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -32,19 +32,19 @@ export default {
                 </p>
             </template>
             <template v-else>
-                <div class="type-title-sm">Creators</div>
+                <div class="type-title-sm">Date of completion</div>
                 <p class="type-body">
                     <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
                         <span >{{ creator }}</span
                         ><span v-if="index < creators.length - 1">, </span>
                     </template>
                 </p>
-                <div class="type-title-sm">Verifier</div>
+                <div class="type-title-sm">Attempt count</div>
                 <p class="type-body">
                     <span>{{ verifier }}</span>
                 </p>
             </template>
-            <div class="type-title-sm">Publisher</div>
+            <div class="type-title-sm">Enjoyment rating</div>
             <p class="type-body">
                 <span>{{ author }}</span>
             </p>
